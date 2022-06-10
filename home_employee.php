9a562b5dbf9289b0504111eb7b287caa46c04353
Rutuu4 <chaudharihrutu.com>
2022-06-10 17:59:44 +0530

    Home Page of Employee Details

---
+++ home_employee.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style>
+        .center {
+            padding: 180px 0;
+            color: #454545;
+            text-align: center;
+            font-family: "Courier, monospace";
+
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <a href="dd_login.php" style="overflow:hidden">
+                    <h4 class="text-white">Login</h4>
+                </a>
+                <a href="dd_regi.php" style="overflow:hidden">
+                    <h4 class="text-white">Registration</h4>
+                </a>
+                <!-- <a href="dd_login.php"><span class="text-muted">Login</span></a><br>
+                    <a href="dd_regi.php"><span class="text-muted">Register</span></a> -->
+            </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
+</body>
+<h1 class="center display-1">Employee Details </h1>
+
+</html>
